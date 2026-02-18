48268da17c057ee34378bf0772d5840989fbf8c4
D1m0nchik <amelkin.dima.5@gmail.com>
2026-02-18 08:18:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import datetime
 
+TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = int(os.getenv("GROUP_ID"))
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+last_schedule = None
